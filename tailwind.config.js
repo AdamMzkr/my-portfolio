4d5cad7a4a0084a5f7a6edfdf374604a656e3665
AdamMzkr <93909097+AdamMzkr@users.noreply.github.com>
2022-11-07 20:47:56 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// prettier-ignore
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#393E46',
+        'text': '#DEDFDF',
+        'accent': '#00FFF5',
+        'secondary': '#00ADB5',
+        'dark': '#1E1E1E'
+      },
+      fontFamily: {
+        'poppins': ["'Popins'", "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+};
